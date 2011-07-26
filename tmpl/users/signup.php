aa080e397a593417e32265579d023ed521d8d3a5
carey.dayrit@gmail.com <carey.dayrit@gmail.com@70100bf1-cd7c-a866-687c-5adc70ff4925>
2011-07-26 14:34:05 +0000

    merged updates from goalfinch

--- tmpl/users/signup.php
+++ tmpl/users/signup.php
@@ -1,14 +1,19 @@
+<div id="signup_form">
 <form method="post" >
 <fieldset>
 <legend accesskey="l">Sign Up</legend>
 
 <label for="username">Username :</label>
 <input type="text" name="username" />
+<br />
 <label for="password">Password :</label>
 <input type="password" name="password" />
+<br />
 <label for="password2">Confirm Password :</label>
 <input type="password" name="password2" />
+<br />
 <input type="submit" value="Sign Up" name="submit" />
 </fieldset>
 
 </form>
+</div>
