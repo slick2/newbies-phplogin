8ab81d868f16c696c0891d4b5ace9aeb13533777
carey.dayrit@gmail.com <carey.dayrit@gmail.com@70100bf1-cd7c-a866-687c-5adc70ff4925>
2011-07-31 17:21:23 +0000

    worked on member list

--- tmpl/users/list.php
+++ tmpl/users/list.php
@@ -1,1 +1,18 @@
 <h2>List Members</h2>
+
+<table>
+<tr>
+<th><input type="checkbox" /></th>
+    <th>Username</th>
+    <th>Action</th>
+</tr>
+<?php while($users=mysql_fetch_array($result)):?>
+<tr>
+<td><input type="checkbox" /></td>
+    <td><?php echo $users['username']?></td>
+    <td><a href="#">Edit</a> | <a href="#">Delete</a></td>
+</tr>
+
+<?php endwhile;?>
+
+</table>
